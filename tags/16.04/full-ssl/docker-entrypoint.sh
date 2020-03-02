533c4a3be18eb670bf3ce93e47ba4b9bfd29682a
taegeun lim <imori333@gmail.com>
2020-03-02 17:33:33 +0900

    fix but

--- tags/16.04/full-ssl/docker-entrypoint.sh
+++ tags/16.04/full-ssl/docker-entrypoint.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
+CONTAINER_NAME=$1
+SERVER_NAME=$2
+PEM_PATH=$3
+KEY_PATH=$4
+
 rm -f /etc/nginx/sites-enabled/default
-sed -ri 's@CONTAINER_NAME@'$1'@' /etc/nginx/sites-available/ssl
-sed -ri 's@SERVER_NAME@'$2'@' /etc/nginx/sites-available/ssl
-sed -ri 's@PEM_PATH@'$3'@' /etc/nginx/sites-available/ssl
-sed -ri 's@KEY_PATH@'$4'@' /etc/nginx/sites-available/ssl
+sed -ri 's@CONTAINER_NAME@'${CONTAINER_NAME}'@' /etc/nginx/sites-available/ssl
+sed -ri 's@SERVER_NAME@'${SERVER_NAME}'@' /etc/nginx/sites-available/ssl
+sed -ri 's@PEM_PATH@'${PEM_PATH}'@' /etc/nginx/sites-available/ssl
+sed -ri 's@KEY_PATH@'${KEY_PATH}'@' /etc/nginx/sites-available/ssl
 # cp -f sites-available/ssl sites-available/default
 ln -s /etc/nginx/sites-available/ssl /etc/nginx/sites-enabled/default
