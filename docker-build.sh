a2d58fe4265ca87696192a74520e633b0c89bc2a
taegeun lim <imori333@gmail.com>
2020-03-02 15:21:30 +0900

    set proxy

---
+++ docker-build.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+docker build -t nginx-cloudfare-ssl-proxy ./tags/16.04/full-ssl/
--- docker-run.sh
+++ docker-run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run -d -v certs:/etc/nginx/certs --name nginx-ssl -p 80:80 --rm ngins-cloudfare-ssl-proxy
+docker run -d -v /home/ubuntu/Docker-nginx-cloudflare-ssl-proxy/certs:/etc/nginx/certs -e SERVER_NAME=docker.havehad.kr --name nginx-ssl -p 443:443 --rm nginx-cloudfare-ssl-proxy
