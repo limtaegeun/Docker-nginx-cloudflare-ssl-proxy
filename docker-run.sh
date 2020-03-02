e81ee6f2daf9d6486de2e300e688cafee695528b
taegeun lim <imori333@gmail.com>
2020-03-02 15:42:04 +0900

    fix example sh

--- docker-run.sh
+++ docker-run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run -d -v /home/ubuntu/Docker-nginx-cloudflare-ssl-proxy/certs:/etc/nginx/certs -e SERVER_NAME=docker.havehad.kr --name nginx-ssl -p 443:443 --rm nginx-cloudfare-ssl-proxy
+dockerun -v /home/ubuntu/Docker-nginx-cloudflare-ssl-proxy/certs:/etc/nginx/certs --name nginx-ssl -p 443:443 -p 80:80 --network nginx-net --rm -d nginx-cloudfare-ssl-proxy
