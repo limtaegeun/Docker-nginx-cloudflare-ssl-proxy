9a49a8e2648aea80467a4f001c7f00d3ab1bf0a0
taegeun lim <imori333@gmail.com>
2020-03-04 15:31:49 +0900

    fix can use host network

--- docker-run.sh
+++ docker-run.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
 docker run -v /home/ubuntu/Docker-nginx-cloudflare-ssl-proxy/certs:/etc/nginx/certs --name nginx-ssl -p 443:443 -p 80:80 --network nginx-net --rm -d nginx-cloudfare-ssl-proxy
+# docker run -v /home/ubuntu/Docker-nginx-cloudflare-ssl-proxy/certs:/etc/nginx/certs --name nginx-ssl --network host --rm -d nginx-cloudfare-ssl-proxy
